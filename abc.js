95df11a9aeb46194257b43bfb9723206652c2337
digitlalearning <digitallearning1505@gmail.com>
2024-01-23 17:59:54 +0530

    asd

--- abc.js
+++ abc.js
@@ -30,8 +30,8 @@ const storage = multer.diskStorage({
          
      })
   })
-  app.get("/",(req,resp)=>{
-    const data=ImageModel.find()
+  app.get("/",async(req,resp)=>{
+    const data=await ImageModel.find()
     resp.send(data)
   })
   app.listen(4000)
